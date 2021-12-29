0253943feaba8abbc010c550607179b35bdfd47d
kai <oneillkai@icloud.com>
2021-12-28 20:37:45 -0800

    add mock spread data

---
+++ src/mock_data/mockSpread.js
@@ -0,0 +1,19 @@
+import { Spread } from 'models/spread'
+import { Card } from 'models/card'
+
+class MockSpread {
+  static basicSpread() {
+    const spread = new Spread('12/28/2021');
+    const card1 = new Card('Two of Swords', 'swords', 2, false);
+    const card2 = new Card('Devil', 'major', 15, true);
+    const card3 = new Card('Eight of Cups', 'cups', 8, false);
+
+    spread.addCard(card1);
+    spread.addCard(card2);
+    spread.addCard(card3);
+
+    return spread;
+  }
+}
+
+export { MockSpread }
